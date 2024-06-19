5750eb3878ad4c8cf4ca7ac4ab4cfb04131cdad3
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2024-06-19 14:11:36 +0900

    Update index.js

---
+++ script/index.js
@@ -0,0 +1,6 @@
+qowieuhd9q73uyrdhq98e7rydhq2987rdyhq287rudyqh2087rdyhq298dr7yq2h0r78dy23h09dr8723h
+rdq230[8rdh2087rdydq298r7d6q2gdr
+    23r0d98q20g9r8d7tqgprd8q2hrd09qw8drhq29rd[
+        2qryqrgd78qyg0dr87qyhr078
+    ]
+]
