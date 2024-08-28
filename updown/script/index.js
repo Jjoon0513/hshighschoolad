5ce7ebc280f9a6bfb2d37f9a3336d26519f10f95
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2024-08-28 14:23:11 +0900

    2024-08-28-0221
    코드 최적화 및 오류 수정.
    글자 레인보우 효과 추가
    그림 수정.
    아니 게이같은거 하지말라고;;;;;;

--- updown/script/index.js
+++ updown/script/index.js
@@ -1,4 +1,4 @@
-let computerNum=0; 
+let computerNum=0;
 let userNum=document.getElementById("user-input");
 let playButton=document.getElementById("play-button");
 let resetButton=document.getElementById("reset-button");
@@ -6,7 +6,6 @@ let resultArea=document.getElementById("result-area");
 let resultNum=document.getElementById("result-Num");
 let chance = 10;
 let chanceArea=document.getElementById("chance-area");
-let gameOver=false;
 let history=[];
 //html 글자 및 버튼
 
@@ -17,15 +16,14 @@ resetButton.addEventListener("click",reset)
 //기회 및 버튼
 
 function randomNum(){
-    computerNum=Math.floor(Math.random()*6974)+1;
-    console.log("랜덤번호", computerNum)
+    computerNum=Math.floor((Math.random() * 6974) + 1);
 }
 //랜덤 값 받기
 
 function playGame(){
-    let userValue=userNum.value
+    var userValue=userNum.value
 
-    if(userValue<1 && userValue>6974){
+    if(userValue<1 || userValue>6974){
         resultArea.textContent="1부터 6974까지의 숫자를 입력해주세요"
         return;
     }
@@ -36,15 +34,15 @@ function playGame(){
 
     chance--;
     chanceArea.textContent=`남은 기회: ${chance}번`;
-    console.log("기회?",chance);
     history.push(userValue);
-    console.log("입력값",history);
     
 
     if(userValue>computerNum){
+        resultArea.style.color = "blue"
         resultArea.textContent="내려";
 
     }else if(userValue<computerNum){
+        resultArea.style.color = "red"
         resultArea.textContent="올려";
     
     }else{
@@ -59,24 +57,17 @@ function playGame(){
         chanceArea.innerHTML="reset버튼을 눌러주세요"
         resultNum.innerText=`답은 ${computerNum}였습니다.`;
         playButton.disabled=true;
-        gameOver = true;
-    }
-    if(gameOver == true){
-        resultArea.textContent="실패하셨군요. 당신은 레즈입니다.";
-        chanceArea.innerHTML="reset버튼을 눌러주세요"
-        resultNum.innerText=`답은 ${computerNum}였습니다.`;
-        playButton.disabled=true;
     }
 }
 //게임의 승패 및 올림과 내림 출력, 남은 기회 등의 게임 활동 내용
 
 function reset(){
+    randomNum();
     userNum.value="";
     resultArea.textContent="1부터 6974까지의 숫자를 입력해주세요";
     chanceArea.innerHTML="남은 기회: 10번";
-    randomNum();
     playButton.disabled = false;
-    gameOver = false;
+    resultArea.style.color = "black";
     chance = 10;
     history = [];
 }
---
+++ updown/script/rainbow.js
@@ -0,0 +1,17 @@
+let rainbow = document.getElementsByClassName("rainbow")[0];
+
+randomv = 0;
+random_color = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'purple']
+
+function rainbowf(){
+    if (randomv == 6){
+        randomv = 0
+    } else {
+        randomv++
+        
+    }
+    rainbow.style.color = random_color[randomv]
+    console.log("잉")
+}
+
+var rainbow_looping = setInterval(rainbowf, 70)
